0e3cb115d9d1677b05ee2425a78b730442ac1cd3
pcreem <creempepper@gmail.com>
2021-02-24 16:48:51 +0800

    add false test function

--- tests/Unit/TheFirstTest.php
+++ tests/Unit/TheFirstTest.php
@@ -7,4 +7,8 @@ class TheFirstTest extends TestCase
     public function testTrueEqualTrue(){
         $this->assertEquals(expected: true, actual: true);
     }
+
+    public function testTrueIsNotEqualFalse(){
+        $this->assertNotEquals(expected: true, actual:false);
+    }
 }
